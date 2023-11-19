7d4a809fb02fdb785cf0b013050d164e7bfd6492
Yasin Usman Afridi <119350611+YasinAfridi07@users.noreply.github.com>
2023-11-19 13:15:19 +0400

    Add files via upload

---
+++ mylib/ft_strrchr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yusman <yusman@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 02:48:38 by yusman            #+#    #+#             */
+/*   Updated: 2022/12/15 15:26:52 by yusman           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)(s + i));
+		i--;
+	}
+	return (NULL);
+}
+// int main ()
+// {
+// 		char i[] = "hello";
+// 	printf("%s", ft_strrchr(i, 'l'));
+// }
